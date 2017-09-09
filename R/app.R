3e9cd5dee23855a749dfce09f8cf163bb1e5ac45
Roel Hogervorst <hogervorst.rm@gmail.com>
2017-09-09 12:05:29 +0200

    added shiny as dependen

---
+++ R/app.R
@@ -0,0 +1,18 @@
+library(powrballad)
+server <- function(input, output) {
+    observeEvent(
+        input$random_song_selector, {
+            output$song <- renderText({select_powerballad()} )
+        }
+    )
+
+    output$song <- renderText({select_powerballad()} )
+}
+
+ui <- fluidPage(
+    actionButton("random_song_selector", "Random Song Selector"),
+            h3(textOutput("song"))
+
+)
+
+shinyApp(ui = ui, server = server)
